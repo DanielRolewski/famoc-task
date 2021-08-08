cf4aefe3a3ba4089e9fe5b1f5fb179a3caae8eaf
DanielRolewski <daniel_rolewski@outlook.com>
2021-08-08 11:34:56 +0200

    Add method getResponse() to get data from api.

--- index.php
+++ index.php
@@ -4,5 +4,13 @@ require 'src/Request.php';
 
 $citiesList = $config['cities'];
 $apiKey = $config['apiKey'];
+
 $request = new Request();
+$objectList = [];
+
+for($i = 0; $i < count($citiesList); $i++) {
+  $url = $request -> makeUrl($apiKey, $citiesList[$i]);
+  $response = $request -> getResponse($url);
+  array_push($objectList, $response);
+}
 ?>
